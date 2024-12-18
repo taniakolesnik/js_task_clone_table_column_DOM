d53d258ed0fda60ee11a7e94fbf2605dc051ac33
Tania Kolesnik <kolesnik@gmail.com>
2024-12-18 11:15:25 +0000

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead > tr');
+const posTitleColCopy = thead.querySelector('th:nth-child(2)').cloneNode(true);
+const lastTitleCol = thead.querySelector('th:last-child');
+
+thead.insertBefore(posTitleColCopy, lastTitleCol);
+
+const rows = document.querySelectorAll('tbody > tr');
+
+rows.forEach((row) => {
+  const newCol = row.querySelector('td:nth-child(2)').cloneNode(true);
+  const lastCol = row.querySelector('td:last-child');
+
+  row.insertBefore(newCol, lastCol);
+});
+
+// thead.tr > new col name
+// tbody.tr > new col woith same value as columgn 2
