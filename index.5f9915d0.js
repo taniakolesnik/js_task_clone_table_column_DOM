e545110ed8a9815217dc2658da6bbfdd58ec9dec
Tania Kolesnik <kolesnik@gmail.com>
2024-12-18 11:21:54 +0000

    make build

---
+++ index.5f9915d0.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("thead > tr"),t=e.querySelector("th:nth-child(2)").cloneNode(!0),r=e.querySelector("th:last-child");e.insertBefore(t,r),document.querySelectorAll("tbody > tr").forEach(function(e){var t=e.querySelector("td:nth-child(2)").cloneNode(!0),r=e.querySelector("td:last-child");e.insertBefore(t,r)});
+//# sourceMappingURL=index.5f9915d0.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const thead = document.querySelector("thead > tr");
+const posTitleCol = thead.children[1].cloneNode(true);
+thead.insertBefore(posTitleCol, thead.lastElementChild);
+document.querySelectorAll("tbody > tr").forEach((row)=>{
+    const posCol = row.children[1].cloneNode(true);
+    row.insertBefore(posCol, row.lastElementChild);
+}); // thead.tr > new col name
+ // tbody.tr > new col woith same value as columgn 2
+
+//# sourceMappingURL=index.f75de5e1.js.map
